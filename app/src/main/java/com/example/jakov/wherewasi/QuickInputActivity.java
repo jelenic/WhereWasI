edc117ab8cdad23754d270ecfc35f9f871130080
jelenic <astro.jakov@gmail.com>
2018-07-11 16:46:14 +0200

    started added one intent

---
+++ app/src/main/java/com/example/jakov/wherewasi/QuickInputActivity.java
@@ -0,0 +1,13 @@
+package com.example.jakov.wherewasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuickInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_input);
+    }
+}
