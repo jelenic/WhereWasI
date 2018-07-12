8ab6d316c349c3baa5bee9927dbe97af3ffc9f20
jelenic <astro.jakov@gmail.com>
2018-07-12 15:59:33 +0200

    added databasehelper with table log_table (columns for time,name,desc,latitude,longitude)

---
+++ app/src/main/java/com/example/jakov/wherewasi/DatabaseHelper.java
@@ -0,0 +1,96 @@
+package com.example.jakov.wherewasi;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "log_table";
+    private static final String COL0 = "timestamp";
+    private static final String COL1 = "name";
+    private static final String COL2 = "description";
+    private static final String COL3 = "latitude";  //North-South
+    private static final String COL4 = "longitude";   //East-West
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context,TABLE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (COL0 TEXT PRIMARY KEY , " +
+                COL1 +" TEXT , " + COL2 + " TEXT, " + COL3 + " TEXT, "+ COL4 + " TEXT  )";
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    public boolean addData(String name,String description, String latitude, String longitude) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        Date currentTime = Calendar.getInstance().getTime();
+        contentValues.put(COL0, currentTime.toString());
+        contentValues.put(COL1, name);
+        contentValues.put(COL2, description);
+        contentValues.put(COL3, latitude);
+        contentValues.put(COL4, longitude);
+
+        Log.d(TAG, "addData: Adding " + name + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        //if date as inserted incorrectly it will return -1
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getNameKeys(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL0 + " FROM " + TABLE_NAME +
+                " WHERE " + COL1 + " = '" + name + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void deleteLog(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL1 + " = '" + id + "'";
+        Log.d(TAG, "deleteName: query: " + query);
+        Log.d(TAG, "deleteName: Deleting " + id + " from database.");
+        db.execSQL(query);
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
+++ app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 
 public class LoggedInActivity extends AppCompatActivity {
@@ -12,8 +13,8 @@ public class LoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
-        Button QuickCheckInBtn=(Button) findViewById(R.id.QuickCheckInBtn);
-        QuickCheckInBtn.setOnClickListener(new View.OnClickListener() {
+        Button QuickInputBtn=(Button) findViewById(R.id.QuickInputBtn);
+        QuickInputBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(LoggedInActivity.this,QuickInputActivity.class);
