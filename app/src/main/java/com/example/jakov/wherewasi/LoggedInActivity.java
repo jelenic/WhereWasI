ebe25d0e8561a58ff2ced4bfc836ff31b871699b
petar19 <petar.lazic@fer.hr>
2018-07-14 18:12:54 +0200

    commit before merge

--- app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
+++ app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
@@ -1,7 +1,10 @@
 package com.example.jakov.wherewasi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoggedInActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class LoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        Button QuickInputBtn=(Button) findViewById(R.id.QuickInputBtn);
+        QuickInputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(LoggedInActivity.this,QuickInputActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Button QuickCheckInBtn=(Button) findViewById(R.id.QuickCheckInBtn);
+        QuickCheckInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int time = 12;
+                int lan = 45;
+                int lon = 87;
+
+
+            }
+        });
+
     }
 }
