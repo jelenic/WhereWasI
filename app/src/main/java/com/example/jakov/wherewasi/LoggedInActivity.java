5ff20209e8c594171447c962b822563077664dbf
jelenic <astro.jakov@gmail.com>
2018-07-17 16:51:54 +0200

    added lattitude and longitude detection added geocoder full adress function

--- app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
+++ app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
@@ -1,18 +1,39 @@
 package com.example.jakov.wherewasi;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class LoggedInActivity extends AppCompatActivity {
     Button QuickCheckInBtn;
     DatabaseHelper logdb;
+    static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    double latituded;
+    double longituded;
+
+
+
 
 
     @Override
@@ -28,6 +49,8 @@ public class LoggedInActivity extends AppCompatActivity {
 
             }
         });
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
 
         Button ViewLogsBtn=(Button) findViewById(R.id.ViewLogsBtn);
         ViewLogsBtn.setOnClickListener(new View.OnClickListener() {
@@ -47,14 +70,39 @@ public class LoggedInActivity extends AppCompatActivity {
 
     }
 
+
+    void getLocation() {
+        if( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if (location != null){
+                latituded = location.getLatitude();
+                longituded = location.getLongitude();
+
+            }
+        }
+
+    }
+
+
+
+
     public void adddata(){
         QuickCheckInBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 //get langitude and longitude
-                String latitude="00,000";
-                String longitude="00,000";
-                boolean insertlog = logdb.addData(null,null,latitude,longitude);
+                getLocation();
+                String latitude=Double.toString(latituded);
+                String longitude=Double.toString(longituded);
+                String name = getCompleteAddressString(latituded,longituded);
+                boolean insertlog = logdb.addData(null,name,latitude,longitude);
                 if (insertlog==true){
                     Toast.makeText(LoggedInActivity.this,"INSERTED",Toast.LENGTH_LONG).show();
 
@@ -63,5 +111,42 @@ public class LoggedInActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("My Cur location address", strReturnedAddress.toString());
+            } else {
+                Log.w("My Cur location address", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("My Cur location address", "Cant get Address!");
+        }
+        return strAdd;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                getLocation();
+                break;
+        }
     }
 }
