acc1ac3944e42de1417dc988a2704b3a2f09172c
petar19 <petar.lazic@fer.hr>
2018-07-19 15:19:07 +0200

    deleted unnecessary stuff

--- app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
+++ app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
@@ -11,19 +11,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -141,7 +138,7 @@ public class LoggedInActivity extends AppCompatActivity {
                 String latitude=Double.toString(latituded);
                 String longitude=Double.toString(longituded);
                 String name = getCompleteAddressString(latituded,longituded);
-                boolean insertlog = logdb.addData(null,name,latitude,longitude);
+                boolean insertlog = logdb.addData(name,null,latitude,longitude);
                 if (insertlog==true){
                     Toast.makeText(LoggedInActivity.this,"INSERTED",Toast.LENGTH_LONG).show();
 
