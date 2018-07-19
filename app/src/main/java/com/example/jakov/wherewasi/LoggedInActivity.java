99e8ca3c21e836891a660bf47c12571f981f495e
jelenic <astro.jakov@gmail.com>
2018-07-19 13:45:46 +0200

    upgraded location, now it works more than one time added location listener only 1 method specified for now comented bunch of old stuff

--- app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
+++ app/src/main/java/com/example/jakov/wherewasi/LoggedInActivity.java
@@ -1,6 +1,7 @@
 package com.example.jakov.wherewasi;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,8 +10,10 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,34 +32,57 @@ public class LoggedInActivity extends AppCompatActivity {
     DatabaseHelper logdb;
     static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
+    LocationListener locationListener;
     double latituded;
     double longituded;
 
 
-
-
-
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
-        Button QuickInputBtn=(Button) findViewById(R.id.QuickInputBtn);
+        Button QuickInputBtn = (Button) findViewById(R.id.QuickInputBtn);
         QuickInputBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(LoggedInActivity.this,QuickInputActivity.class);
+                Intent intent = new Intent(LoggedInActivity.this, QuickInputActivity.class);
                 startActivity(intent);
 
             }
         });
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        getLocation();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("LoggedInActivity:", location.toString());
+                latituded = location.getLatitude();
+                longituded = location.getLongitude();
 
-        Button ViewLogsBtn=(Button) findViewById(R.id.ViewLogsBtn);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        //getLocation();
+
+        Button ViewLogsBtn = (Button) findViewById(R.id.ViewLogsBtn);
         ViewLogsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(LoggedInActivity.this,LogViewActivity.class);
+                Intent intent = new Intent(LoggedInActivity.this, LogViewActivity.class);
                 startActivity(intent);
 
             }
@@ -64,14 +90,26 @@ public class LoggedInActivity extends AppCompatActivity {
 
 
         logdb = new DatabaseHelper(this);
-        QuickCheckInBtn= (Button) findViewById(R.id.QuickCheckInBtn);
+        QuickCheckInBtn = (Button) findViewById(R.id.QuickCheckInBtn);
         adddata();
 
+        if (Build.VERSION.SDK_INT < 23) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
 
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+                return;
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
     }
 
 
-    void getLocation() {
+    /*void getLocation() {
         if( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -81,6 +119,7 @@ public class LoggedInActivity extends AppCompatActivity {
         } else {
             Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
+
             if (location != null){
                 latituded = location.getLatitude();
                 longituded = location.getLongitude();
@@ -88,7 +127,7 @@ public class LoggedInActivity extends AppCompatActivity {
             }
         }
 
-    }
+    }*/
 
 
 
@@ -98,7 +137,7 @@ public class LoggedInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 //get langitude and longitude
-                getLocation();
+                //getLocation();
                 String latitude=Double.toString(latituded);
                 String longitude=Double.toString(longituded);
                 String name = getCompleteAddressString(latituded,longituded);
@@ -143,10 +182,15 @@ public class LoggedInActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch (requestCode) {
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+        /*switch (requestCode) {
             case REQUEST_LOCATION:
                 getLocation();
                 break;
-        }
+        }*/
     }
 }
