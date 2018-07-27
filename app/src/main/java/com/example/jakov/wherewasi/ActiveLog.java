430e43f567c8f2dfc0a9654c1761a77fb1cc58eb
petar19 <petar.lazic@fer.hr>
2018-07-27 14:52:03 +0200

    commit before merge 27.7

---
+++ app/src/main/java/com/example/jakov/wherewasi/ActiveLog.java
@@ -0,0 +1,23 @@
+package com.example.jakov.wherewasi;
+
+public class ActiveLog {
+    private static ActiveLog instance;
+    private String val="Default log";
+
+    public static ActiveLog getInstance() {
+        if (instance == null)
+            instance = new ActiveLog();
+        return instance;
+    }
+
+    private ActiveLog() {
+    }
+
+    public String getValue() {
+        return val;
+    }
+
+    public void setValue(String value) {
+        this.val = value;
+    }
+}
